294803777ad1953aca8024be678094afc9a0d1ee
sou-uemura <soh.mtw@gmail.com>
2019-07-17 13:42:04 +0700

    質問詳細の表示

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -1,2 +1,30 @@
-<h1>{{ $question->title }}</h1>
-<p>{{ $question->content }}</p>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">質問詳細</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $question->title }}</h5>
+                                <h5 class="card-title">
+                                  投稿者:{{ $question->user->name }}
+                                </h5>
+                                <p class="card-text">{{ $question->content }}</p>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
